d8429640a83817cbb1a8bd8c50f4236cc2b7e049
Cesar Acual <augustoacual@gmail.com>
2019-11-08 00:10:25 -0600

    4 - Interaccion entre objetos

--- instance.php
+++ instance.php
@@ -1,7 +1,12 @@
 <?php
 
+function show($message)
+{
+    echo "<p>$message</p>";
+}
+
 abstract class Unit {
-    protected $alive = true;
+    protected $hp = 40;
     protected $name;
 
     public function __construct($name)
@@ -9,30 +14,86 @@ abstract class Unit {
         $this->name = $name;
     }
 
-    public function move($direction)
+    public function getName()
     {
-        echo "<p>{$this->name} camina hacia $direction</p>";
+        return $this->name;
     }
 
-    abstract public function attack($opponent);
+    public function getHp()
+    {
+        return $this->hp;
+    }
+
+    public function move($direction)
+    {
+        show("{$this->name} camina hacia $direction");
+    }
+
+    abstract public function attack(Unit $opponent);
+
+    public function takeDamage($damage)
+    {
+        $this->setHp($this->hp - $damage);
+
+        if ($this->hp <= 0) {
+            $this->die();
+        }
+    }
+
+    public function die()
+    {
+        show("{$this->name} muere");
+    }
+
+    private function setHp($points)
+    {
+        $this->hp = $points;
+
+        show("{$this->name} ahora tiene {$this->hp} puntos de vida");
+    }
 }
 
 class Soldier extends Unit
 {
-    public function attack($opponent)
+    protected $damage = 40;
+
+    public function attack(Unit $opponent)
     {
-        echo "<p>{$this->name} corta a $opponent en dos</p>";
+        show("{$this->name} corta a {$opponent->getName()} en dos");
+
+        $opponent->takeDamage($this->damage);
+    }
+
+    public function takeDamage($damage)
+    {
+        parent::takeDamage($damage / 2);
     }
 }
 
 class Archer extends Unit
 {
-    public function attack($opponent)
+    protected $damage = 20;
+
+    public function attack(Unit $opponent)
     {
-        echo "<p>{$this->name} dispara una flecha a $opponent</p>";
+        show("{$this->name} dispara una flecha a {$opponent->getName()}");
+
+        $opponent->takeDamage($this->damage);
+    }
+
+    public function takeDamage($damage)
+    {
+        if (rand(0, 1)) {
+            parent::takeDamage($damage);
+        }
     }
 }
 
+$ramm = new Soldier('Ramm');
+
 $ducke = new Archer('Ducke');
 //$ducke->move('el norte');
-$ducke->attack('Ramm');
+$ducke->attack($ramm);
+$ducke->attack($ramm);
+
+$ramm->attack($ducke);
